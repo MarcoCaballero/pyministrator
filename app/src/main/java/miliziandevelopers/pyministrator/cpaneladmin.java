dc16cb5a24a6a84432c5d469fccc63299c284f84
peloxoo <peloxoo@gmail.com>
2017-01-16 20:02:19 +0100

    A ver si funciona

--- app/src/main/java/miliziandevelopers/pyministrator/cpaneladmin.java
+++ app/src/main/java/miliziandevelopers/pyministrator/cpaneladmin.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class cpaneladmin extends AppCompatActivity
+    //Si ves esto es que estamos connected
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
